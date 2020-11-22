8e474a13b0cee3c2559a1e628718bdd579b5831e
smileyy39 <74601585+smileyy39@users.noreply.github.com>
2020-11-22 15:40:08 +0000

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# creating necessary .config/Red-DiscordBot directory
+mkdir ~/.config
+mkdir ~/.config/Red-DiscordBot
+
+# extracting HOST, PORT, USERNAME, PASSWORD and DB_NAME from MONGODB_URI
+# echo $MONGODB_URI
+
+# regex="mongodb:\/\/([a-z0-9_]+):([a-z0-9]+)@([a-z0-9.]+):(\d+)\/([a-z0-9_]+)"
+
+echo $CONFIG_JSON > ~/.config/Red-DiscordBot/config.json
+
+python3 -m redbot heroku --token $TOKEN --prefix $PREFIX $OPTIONAL_ARGS
